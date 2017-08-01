580f0e2ba0b4ed77bfff7ddd84f055c2f667af99
Luis Hernandez <lhernandez@data-keeper.com>
2017-08-01 16:00:17 -0500

    Adding Shell to the repository

---
+++ DBExportAgent/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLServerDBManager;
+
+namespace DBExportAgent
+{
+    class Program
+    {
+        /*
+         * Steps to complete:
+         * 1: Create a NEW database from a BAK file
+         * 2: Delete unneeded artifacts: SPs, Fns, triggers, views, synonyms, indexes, foreign keys, constraints
+         * 3: Run customized SP (to delete unneeded rows and tables)
+         * 4: Backup NEW database to a BAK file
+         * 5: Drop NEW database
+         * 6: Delete exiting physical files for NEW database
+         */
+        static void Main(string[] args)
+        {
+            string SAConnectionString = ""; //This it to use the System Administrator credentials
+            string DBOwnerConnectionString = ""; //This it to use the owner of the new DB credentials. QUESTION: Can they be the same? Should they?
+            string ProductionBackupFile = "";
+            string ResultingBackupFile = "";
+            string NewDBName = "";
+            string Script = "";
+
+            int result = DBManager.RestoreDB(SAConnectionString, ProductionBackupFile, NewDBName);
+
+            if (result == 0)
+            {
+                result = DBManager.ClearDB(DBOwnerConnectionString, NewDBName);
+
+                if (result == 0)
+                {
+                    result = DBManager.ExecuteScript(DBOwnerConnectionString, NewDBName, Script);
+                    if (result == 0)
+                    {
+                        result = DBManager.BackupDB(SAConnectionString, NewDBName, ResultingBackupFile);
+                        if (result == 0)
+                        {
+                            result = DBManager.DropDB(SAConnectionString, NewDBName);
+                            if (result == 0)
+                            {
+                                //SUCCESS!!!
+                            }
+                            else
+                            {
+                                //Log error
+                            }
+                        }
+                        else
+                        {
+                            //Log error
+                        }
+                    }
+                    else
+                    {
+                        //Log error
+                    }
+                }
+                else
+                {
+                    //Log error
+                }
+            }
+            else
+            {
+                //Log error
+            }
+        }
+    }
+}
---
+++ SQLServerDBManager/DBManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLServerDBManager
+{
+    public class DBManager
+    {
+        public static int RestoreDB(string ConnectionString, string SourceBAKFile, string NewDBName)
+        {
+            return 0;
+        }
+
+        public static int BackupDB(string ConnectionString, string DBName, string DestinationBAKFile)
+        {
+            return 0;
+        }
+
+        public static int DropDB(string ConnectionString, string DBName)
+        {
+            //Step 1: Get the names of physical files for DB
+            List<string> physicalFiles = new List<string>();
+            //Setp 2: Drop the DB from the SQL Server instance
+            //Step 3: Remove physical files from computer
+            if (RemoveDBFiles(physicalFiles) == 0)
+            {
+            }
+            else
+            {
+                //Log error
+            }
+            return 0;
+        }
+
+        private static int RemoveDBFiles(List<string> DBFiles)
+        {
+            //This function should remove the DB files from the server
+            return 0;
+        }
+
+        public static int ClearDB(string ConnectionString, string DBName
+            , bool deleteSPs = true, bool deleteFns = true, bool deleteViews = true, bool deleteTriggers = true, bool deleteFKs = true, bool deleteSynonims = true)
+        {
+            return 0;
+        }
+
+        public static int ExecuteScript(string ConnectionString, string DBName, string sqlScript)
+        {
+            return 0;
+        }
+    }
+}
