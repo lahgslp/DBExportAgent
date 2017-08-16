8546a2aa6ae80b11c1edf4963fc76f23975981ca
jacamaleon <jacamaleon@hotmail.com>
2017-08-15 22:58:19 -0500

    Proyecto Registrador incluido

---
+++ Registrador/IRegistroEjecucion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registrador
+{
+    public interface IRegistroEjecucion
+    {
+        void Registrar(string evento);
+        void RegistrarError(string error);
+        void RegistrarAdvertencia(string advertencia);
+        List<string> Errores();
+        List<string> Advertencias();
+    }
+}
---
+++ Registrador/RegistroEjecucionArchivo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Registrador
+{
+    public class RegistroEjecucionArchivo : IRegistroEjecucion
+    {
+        string NombreArchivo;
+        List<string> ListaErrores;
+        List<string> ListaAdvertencias;
+
+        public RegistroEjecucionArchivo(string nombreArchivo)
+        {
+            NombreArchivo = nombreArchivo;
+            ListaErrores = new List<string>();
+            ListaAdvertencias = new List<string>();
+        }
+
+        public void Registrar(string evento)
+        {
+            if (NombreArchivo != String.Empty)
+            {
+                //string folder = Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData) + "\\" + "GeneradorASNWin";
+                string ApplicationName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+                string folder = Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData) + "\\" + ApplicationName;
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+                string archivo = folder + "\\" + NombreArchivo + ".txt";
+                if (!File.Exists(archivo))
+                {
+                    //Si archivo no existe, se crea con mensaje inicial de registro:
+                    using (StreamWriter archivoRegistroNuevo = File.CreateText(archivo))
+                    {
+                        archivoRegistroNuevo.WriteLine(evento);
+                    }
+                }
+                else
+                {
+                    using (StreamWriter archivoRegistro = File.AppendText(archivo))
+                    {
+                        archivoRegistro.WriteLine(evento);
+                    }
+                }
+            }
+        }
+
+        public void RegistrarError(string error)
+        {
+            string textoError = "ERROR: " + error;
+            Registrar(textoError);
+            ListaErrores.Add(textoError);
+        }
+
+        public List<string> Errores()
+        {
+            return ListaErrores;
+        }
+
+        public void RegistrarAdvertencia(string advertencia)
+        {
+            string textoAdvertencia = "ADVERTENCIA: " + advertencia;
+            Registrar(textoAdvertencia);
+            ListaAdvertencias.Add(textoAdvertencia);
+        }
+
+        public List<string> Advertencias()
+        {
+            return ListaAdvertencias;
+        }
+    }
+}
